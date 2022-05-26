5b156bf291488f7adfcc5f46d90b9aaa456c11a9
draginoff <93340583+draginoff@users.noreply.github.com>
2022-05-25 21:33:16 -0400

    Completed Arrays Exercises

--- exercises/array-exercises/js/array.js
+++ exercises/array-exercises/js/array.js
@@ -8,15 +8,15 @@ class ArrayFunctions {
    * indexOf([1,2,3,5,6], 3); // 2
    */
   indexOf(arr, item) {
-    if(item + 2 >arr.length){
-      indexArrays = -1;
+    if(item + 1 >arr.length){
+      return -1;
      }
      else
      {
       item = item - 2;
-      indexArrays = arr[item];
+      return arr[item];
      }
-    return indexArrays;
+    
   }
 
   /**
@@ -44,11 +44,11 @@ class ArrayFunctions {
    * filter([1,2,3,5,6,2,4,2], 2); // [1,3,5,6,4]
    */
   filterOut(arr, item) {
-    filtered = arr.filter(function(items)
+    arr = arr.filter(function(items)
     {
     return items != item;
     });
-    return filtered;
+    return arr;
   }
 
   /**
@@ -60,7 +60,7 @@ class ArrayFunctions {
    * append([1,2,3], 4); // [1,2,3,4]
    */
   append(arr, item) {
-    // hint: push, slice, splice would all work. Pay attention to the return value in the docs
+    arr.push(item);
     return arr;
   }
 
@@ -73,7 +73,8 @@ class ArrayFunctions {
    * concat([1,2,3], [4,5,6]); // [1,2,3,4,5,6]
    */
   concat(arr1, arr2) {
-    return ;
+    arr1 = arr1.concat(arr2);
+    return arr1;
   }
 
   /**
@@ -84,7 +85,7 @@ class ArrayFunctions {
    * square([1,2,4,5]); // [1,4,16,25]
    */
   square(arr) {
-    // hint: use .map
-    return ;
+    arr= arr.map(x => Math.pow(x, 2));
+    return arr;
   }
 }
